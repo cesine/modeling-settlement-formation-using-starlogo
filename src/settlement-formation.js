b3ae9b4529dfb40bf5d0679594301292867ef449
cesine <cesine@yahoo.com>
2021-04-18 15:32:20 -0400

    make load in netlogo

---
+++ src/settlement-formation.js
@@ -0,0 +1,60 @@
+{
+  "version": "beta3.0",
+  "otherInfo": {},
+  "widgets": [{
+    "id": 0,
+    "type": "ToggleButtonWidget",
+    "name": "forever",
+    "position": {
+      "x": 19,
+      "y": 77
+    },
+    "size": {
+      "width": 80,
+      "height": 28
+    }
+  }, {
+    "id": 1,
+    "type": "PushButtonWidget",
+    "name": "setup",
+    "position": {
+      "x": 19,
+      "y": 55
+    },
+    "size": {
+      "width": 80,
+      "height": 28
+    }
+  }, {
+    "id": 2,
+    "type": "DataBoxWidget",
+    "name": "Data",
+    "value": " ",
+    "position": {
+      "x": 19,
+      "y": 100
+    },
+    "size": {
+      "width": 166,
+      "height": 51
+    }
+  }],
+  "agents": "",
+  "breeds": [{
+    "name": "The World",
+    "customTraits": [],
+    "blocks": []
+  }, {
+    "name": "Everyone",
+    "customTraits": [],
+    "blocks": []
+  }, {
+    "name": "Turtle",
+    "color": "0xFFFFFF",
+    "shape": "Cube",
+    "size": "1",
+    "customTraits": [],
+    "blocks": []
+  }],
+  "sounds": []
+}
